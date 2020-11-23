00f8193747d17ec893b95704632894a460caa620
Cédric Da Fonseca <captain.spof@gmail.com>
2020-11-23 10:30:37 +0100

    oopsy: add missing files

---
+++ infra/tf-shared/main.tf
@@ -0,0 +1,235 @@
+# Declare the aws provider
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "click-count-tfstate"
+    key    = "net/terraform.tfstate"
+    region = "eu-west-3"
+  }
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+# resource "aws_vpc" "click_count" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
+
+#   tags = {
+#     Name = "vpc-${var.env}"
+#     Env  = var.env
+#   }
+# }
+
+# resource "aws_subnet" "click_count_subnet_a" {
+#   vpc_id     = aws_vpc.click_count.id
+#   cidr_block = "10.0.0.0/24"
+
+#   tags = {
+#     Name = "subnet-a-${var.env}"
+#     Env  = var.env
+#   }
+# }
+
+# resource "aws_subnet" "click_count_subnet_b" {
+#   vpc_id     = aws_vpc.click_count.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "subnet-b-${var.env}"
+#     Env  = var.env
+#   }
+# }
+
+resource "aws_security_group" "sg-allow-web-lb" {
+  name        = "${var.project_name}-${var.stack}-shared-web-ssh-lb"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "Web from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name     = "sg-allow-web-lb"
+    Env      = "shared"
+    Provider = "terraform"
+  }
+}
+
+resource "aws_security_group" "sg-allow-web-ssh-eb" {
+  name        = "${var.project_name}-${var.stack}-shared-web-ssh"
+  description = "Allow Web and SSH inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "Web from lb"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.sg-allow-web-lb.id]
+  }
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name     = "sg-allow-web-ssh-eb"
+    Env      = "shared"
+    Provider = "terraform"
+  }
+}
+
+resource "aws_security_group" "sg-allow-redis" {
+  name        = "allow_redis"
+  description = "Allow Redis inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description     = "Redis from ${aws_security_group.sg-allow-web-lb.name}"
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg-allow-web-ssh-eb.id]
+  }
+
+  ingress {
+    description = "All from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "sg-allow-redis"
+    Env         = var.env
+    Provisioner = "terraform"
+  }
+}
+
+
+##
+## IAM - Role / Policy
+##
+
+resource "aws_iam_role" "beanstalk_ec2" {
+  # name               = "beanstalk-ec2-role"
+  name               = "beanstalk-ec2-user"
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+  tags = {
+    Env         = "shared"
+    Project     = var.project_name
+    Provisioner = "terraform"
+  }
+}
+
+resource "aws_iam_instance_profile" "beanstalk_ec2" {
+  name = "beanstalk-ec2-user"
+  role = aws_iam_role.beanstalk_ec2.name
+}
+
+resource "aws_iam_role" "beanstalk_service" {
+  name               = "beanstalk-service-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "elasticbeanstalk.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "elasticbeanstalk"
+        }
+      }
+    }
+  ]
+}
+EOF
+  # tags               = local.tags
+
+  tags = {
+    Env         = "shared"
+    Project     = var.project_name
+    Provisioner = "terraform"
+  }
+}
+
+resource "aws_iam_policy_attachment" "beanstalk_service" {
+  name       = "elastic-beanstalk-service"
+  roles      = [aws_iam_role.beanstalk_service.id]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
+}
+
+resource "aws_iam_policy_attachment" "beanstalk_service_health" {
+  name       = "elastic-beanstalk-service-health"
+  roles      = [aws_iam_role.beanstalk_service.id]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
+}
+
+resource "aws_iam_policy_attachment" "beanstalk_ec2_worker" {
+  name       = "elastic-beanstalk-ec2-worker"
+  roles      = [aws_iam_role.beanstalk_ec2.id]
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
+}
+
+resource "aws_iam_policy_attachment" "beanstalk_ec2_docker" {
+  name       = "elastic-beanstalk-ec2-docker"
+  roles      = [aws_iam_role.beanstalk_ec2.id]
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
+}
